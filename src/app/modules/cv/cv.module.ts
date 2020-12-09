991d3a106fd9c5a41daebf81c94915192de7dc75
BernardoMB <bmondragonbrozon@gmail.com>
2020-12-08 20:43:36 -0600

    Added Visual Studio Developer Community tab navigation bar

--- src/app/modules/cv/cv.module.ts
+++ src/app/modules/cv/cv.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CvRoutingModule } from './cv-routing.module';
 import { CvRootComponent } from './components/cv-root/cv-root.component';
 import { CvComponent } from './components/cv/cv.component';
 import { ResumeComponent } from './components/resume/resume.component';
-
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [CvRootComponent, CvComponent, ResumeComponent],
   imports: [
     CommonModule,
-    CvRoutingModule
+    CvRoutingModule,
+    MatTabsModule,
+    MatIconModule
   ]
 })
 export class CvModule { }
