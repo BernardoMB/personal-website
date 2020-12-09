034ae03e546e34455a484a615a0a3179698b2c44
BernardoMB <bmondragonbrozon@gmail.com>
2020-12-08 18:32:38 -0600

    Added jumbotron image

--- src/app/modules/cv/cv-routing.module.ts
+++ src/app/modules/cv/cv-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CvRootComponent } from './components/cv-root/cv-root.component';
+import { CvComponent } from './components/cv/cv.component';
+import { ResumeComponent } from './components/resume/resume.component';
 
 const routes: Routes = [
-  { path: '', component: CvRootComponent }
+  {
+    path: '',
+    component:
+    CvRootComponent,
+    children: [
+      { path: '', redirectTo: 'cv', pathMatch: 'full' },
+      { path: 'cv', component: CvComponent },
+      { path: 'resume', component: ResumeComponent },
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/modules/cv/cv.module.ts
+++ src/app/modules/cv/cv.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { CvRoutingModule } from './cv-routing.module';
 import { CvRootComponent } from './components/cv-root/cv-root.component';
+import { CvComponent } from './components/cv/cv.component';
+import { ResumeComponent } from './components/resume/resume.component';
 
 
 @NgModule({
-  declarations: [CvRootComponent],
+  declarations: [CvRootComponent, CvComponent, ResumeComponent],
   imports: [
     CommonModule,
     CvRoutingModule
