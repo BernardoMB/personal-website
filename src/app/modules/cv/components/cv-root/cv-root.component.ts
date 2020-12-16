4b85307fac128aa59bf577dfb99cecaaec619519
BernardoMB <bmondragonbrozon@gmail.com>
2020-12-15 19:14:44 -0600

    Added styls fo Resume entries

--- src/app/modules/cv/components/cv-root/cv-root.component.ts
+++ src/app/modules/cv/components/cv-root/cv-root.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-cv-root',
@@ -7,14 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CvRootComponent implements OnInit {
   showDynamicDisplayContent: boolean = false;
+  name = 'Bernardo Mondragon Brozon';
+  date = moment().format('MM/DD/YYYY');
 
   // NgClass directive example
   isPurple = true;
   isBlue = false;
 
+  // NgStyle directive example
+  leftPanelFontColor = 'white';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeLeftPanelStyle(color: string): void {
+    switch (color) {
+      case 'purple':
+        this.isPurple = true; this.isBlue = false; this.leftPanelFontColor = 'white';
+        break;
+      case 'blue':
+        this.isPurple = false; this.isBlue = true; this.leftPanelFontColor = 'white';
+        break;
+      case 'gray':
+        this.isPurple = false; this.isBlue = false; this.leftPanelFontColor = 'black';
+        break;
+      default: // Optional
+        // purple case
+        this.isPurple = true; this.isBlue = false; this.leftPanelFontColor = 'white';
+        break;
+    }
   }
 
 }
