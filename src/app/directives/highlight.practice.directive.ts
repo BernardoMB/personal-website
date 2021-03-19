d8311f032652c0ae4f9af0bf6ed012846aff9511
BernardoMB <bmondragonbrozon@gmail.com>
2021-03-18 18:31:40 -0600

    Added better styles

--- src/app/directives/highlight.practice.directive.ts
+++ src/app/directives/highlight.practice.directive.ts
@@ -6,9 +6,7 @@ import { Directive, ElementRef, OnInit } from "@angular/core";
 export class BasicHightlightDirective implements OnInit {
   constructor(
     private elementRef: ElementRef // Access to the element this directive sits on
-  ) {
-
-  }
+  ) { }
 
   ngOnInit(): void {
     this.elementRef.nativeElement.style.backgroundColor = 'green';
