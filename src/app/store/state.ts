fa489b49598609b0080ddd73c469ef8bbea120a3
Bernardo.Mondragon <bernardo.mondragon@mx.zurich.com>
2021-05-05 18:21:51 -0500

    Added store to solution

---
+++ src/app/store/state.ts
@@ -0,0 +1,7 @@
+export interface State {
+    initials: 'BMB',
+    name: 'Bernardo Mondragón Brozon',
+    email: 'bmondragonbrozon@gmail.com',
+    phoneNumer: '525535592033',
+    address: 'Paseo San Agustin 78, Lomas Verdes, Naucalpan, Edo. México, Mexico, CP. 53120'
+}
