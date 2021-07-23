a760f08e0a83f6e58baecaf6da0d1ec9733d6737
Bernardo.Mondragon <bernardo.mondragon@mx.zurich.com>
2021-07-23 11:37:00 -0500

    Added translations to about module

--- src/app/modules/about/components/biography/biography.component.ts
+++ src/app/modules/about/components/biography/biography.component.ts
@@ -8,10 +8,14 @@ import * as moment from 'moment';
 })
 export class BiographyComponent implements OnInit {
   readTime = 5 * 60 * 1000; // 5 minutes
+  yearsOfExperience: number | undefined;
+  teachingYears: number | undefined;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.yearsOfExperience = new Date().getFullYear() - 2013;
+    this.teachingYears = new Date().getFullYear() - 2019;
   }
 
 }
